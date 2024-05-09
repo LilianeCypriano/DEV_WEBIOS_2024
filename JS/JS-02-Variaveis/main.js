8a0524107fe6e75e8c2317c17beebe2a00e945c1
lilianeecypriano@gmail.com <lilianeecypriano@gmail.com>
2024-05-08 21:45:35 -0300

    Aula 02 de Java

---
+++ JS/JS-02-Variaveis/main.js
@@ -0,0 +1,45 @@
+//*Objeto Console 
+console.log("Hello Word");
+//Quando trabalha com o texto coloca o mesmo dento de aspas duplas ou simples
+
+//Variaveis
+//Quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+//var pode usar mais de uma vez, á o let não permite isso
+
+var _felippe = 8;
+_felippe = "Messi"
+
+let letsgo = 4
+// let letsgo = 3
+console.log(letsgo)
+
+console.log(_felippe);
+
+console.clear()
+//Limpa o que está acima
+
+//const
+const PI =3.1415;
+
+console.log(PI) 
+
+console.clear
+
+let NAN = 0 / 0
+
+console.log(NAN)
+
+console.clear()
+
+let IOS = "Instituto da Oportunidade Social"
+// String = texto
+
+console.log(" Instituto " + " da " + " Oportunidade " + " Social ")
+
+
+var numero1 = 1
+var texto = " Eu Tenho "
+var texto2 = " reais "
+
+console.log(texto + numero1 + texto2)
+//concatenar = é possiveel concatenar Strings ou concatenar variaveis
