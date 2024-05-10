8e96bb87718d26526a674cfd184d3ad75f1483de
lilianeecypriano@gmail.com <lilianeecypriano@gmail.com>
2024-05-09 21:34:20 -0300

    Aula3 de JS

--- JS/JS-02-Variaveis/main.js
+++ JS/JS-02-Variaveis/main.js
@@ -43,3 +43,44 @@ var texto2 = " reais "
 
 console.log(texto + numero1 + texto2)
 //concatenar = é possiveel concatenar Strings ou concatenar variaveis
+
+console.clear()
+// Valores Booleanos = comprar uma coisa coma outra e se é verdadeiro ou falso 
+
+var num1 = 1
+var num2 = 1
+// num1 é igual a num2
+// 1 é igual a 5 = true
+
+console.log (num1 == num2)
+
+var teste;
+
+console.log(teste)
+
+//Operadores
+var operador1 =8;
+var operador2= null;
+
+console.log (operador1 * operador2); //8 * null
+
+var operador3 = "5"
+var operador4 = 1
+
+console.log(operador3 - operador4); //"5" - 1
+
+var operador5 = "5"
+var operador6 = 1
+
+console.log(operador5 + operador6); //"5" + 1
+
+var operador7 = "five"
+var operador8 = 2
+
+console.log(operador7 * operador8) // "five" * 2 = NaN
+
+var valor1 = 2
+var valor2 = 2
+var resultado = valor1 + valor2
+
+console.log(resultado)
