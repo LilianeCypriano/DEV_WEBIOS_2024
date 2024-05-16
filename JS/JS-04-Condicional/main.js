e5a2a9e1b417c3a5b8ce7602c25dcf5ecd9a468e
lilianeecypriano@gmail.com <lilianeecypriano@gmail.com>
2024-05-15 21:41:33 -0300

    Aula 05

--- JS/JS-04-Condicional/main.js
+++ JS/JS-04-Condicional/main.js
@@ -32,13 +32,39 @@ console.clear()
 let mes = "JANEIRO"
 
 if(mes == "JANEIRO"){
-    console.log("É verao")
-}else if (mes == "JUNHO"){
+    console.log("É verao")}
+    else if (mes == "JUNHO")  {
     console.log("É outono")
-}else if (mes == "JUNHO"){
+}else if (mes == "OUTUBRO"){
     console.log("É primarvera")
 }else{
     console.log("É inverno")
 }
+
+console.clear()
+// let mes = "JANEIRO"
+// Precisa do Break para dá o ponto de partida
+switch(mes){// guardo a expressão que quero validar
+    case  "JANEIRO":
+        console.log("É Verao")
+        break;
+
+    case "Junho":
+        console.log("É outono")
+        break;
+
+    case  "Outono":
+        console.log("É primavera")
+        break;
+
+    default:
+        console.log("É inverno")
+        break;
+
+}
+
+
+
+
 
 
---
+++ JS/JS-05-Repetição/main.js
@@ -0,0 +1,19 @@
+//while
+//enquanto 10 < 0 vou apresentar a mensagem 0 ainda é menor que 10
+
+// var contador = 0
+
+// while(contador <=   10){
+// console.log(contador + "0 ainda é menor que 10")
+// debugger // trava e vê o passo a passo do codigo aparecendo
+// contador++; // contador = contador + 1
+
+// }
+
+var contador = 0
+
+do{
+    debugger
+    console.log(contador + " ainda é menor que 10 ")
+    contador++
+}while(contador < 10)
