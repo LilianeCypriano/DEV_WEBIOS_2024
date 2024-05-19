5839651f9a6cadf67914afe8b84b5c097cd20df5
Liliane Cypriano <lilianeecypriano@gmail.com>
2024-05-18 21:18:44 -0300

    Exercicio Casa

---
+++ JS/JS-02-Variaveis/JS-03-String/Exercicio Casa/main.js
@@ -0,0 +1,10 @@
+var string = "Estamos Apredendo JavaScript e seremos futuros Devs"
+
+console.log(string[19])
+console.log(string)
+console.log(`Maisculo:${string.toUpperCase()}`)
+console.log(string.substring(47))
+
+
+
+
---
+++ JS/JS-05-Repetição/Exercicio Casa/main.js
@@ -0,0 +1,10 @@
+var tabuada = 1
+
+while (tabuada <= 10){
+    var valor = tabuada * 2
+    console.log( " 2 " + " x " + tabuada + " = " + valor )
+    tabuada++
+}
+
+
+
