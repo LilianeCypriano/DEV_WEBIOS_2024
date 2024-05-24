4139644eaf00ae4aecff7d0a8d1361ad1886e1d0
lilianeecypriano@gmail.com <lilianeecypriano@gmail.com>
2024-05-23 21:39:59 -0300

    Aula08

---
+++ JS/JS08-funcoes/main.js
@@ -0,0 +1,51 @@
+function soma(num1, num2, num3){
+    var num1 
+    var num2;
+    var num3;
+    
+
+     return num1 + num2;
+     
+    //  if(e_mail++ "liliane.."  && senha: 1234){
+    //     return URL} else{
+
+    //     }
+
+}
+    
+    console.log(soma(2, 3, 2)) //com
+    // console.log(soma) //sem parentese
+
+
+    //crie uma função que receba dois valores e retorne apenas a media desses valores
+
+   console.clear()
+
+   function media(valor1, valor2){
+    var valor1;
+    var valor2;
+   
+return (valor1 + valor2) /4;
+}
+
+console.log(media(1, 2))
+
+console.clear()
+
+const media2 = (n1, n2, n3, n4) => {
+    return(n1, n2, n3, n4) /4
+}
+
+console.log(media2(4,5,6,7))
+
+console.clear()
+
+function onloadTela() {
+    alert("Sua Tela está carregada")
+    console.log("Recarregou a tela")
+}
+
+const clique =  () =>{
+    alert("Bem-vindo você clicou em mim")
+    console.log("Você clicou em um botão")
+}
