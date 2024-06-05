2ffea9397812043d2d96476a1f9055e92ae2cf34
lilianeecypriano@gmail.com <lilianeecypriano@gmail.com>
2024-06-04 21:31:43 -0300

    Aula11

---
+++ JS/JS11-DOM-Parte1/main.js
@@ -0,0 +1,15 @@
+   //Dom
+   //Link com o arquivo HTML
+   //Acessar o dom
+
+   //se for 1 > São Paulo (cor de fundo vermellho) senão 0 Corinthians (cor de fundo preto)
+   var time = 1
+   if ( time == 1 ){
+    document.body.style.background = "red"
+   }else if ( time == 0){
+    document.body.style.background = "Black"
+   }else{
+    document.body.style.background = "green"
+   }
+
+   window.confirm ("Bem Vindo");
